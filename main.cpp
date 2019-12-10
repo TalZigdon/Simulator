9bf7fb089560088ec9031030d814bcde4c1cc6b4
TalZig <talz2347@gmail.com>
2019-12-10 08:32:48 -0800

    start making laxer function

--- main.cpp
+++ main.cpp
@@ -4,18 +4,43 @@
 #include "unordered_map"
 #include "vector"
 
-
-
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   //create map here
 
   return 0;
 }
 
-vector<string> lexer (FILE* f) {
+vector<string> lexer(string fileName) {
   // create array of strings
+  int i = 0;
+  string line, temp = "";
+  vector<string> vec;
+  fstream file;
+  file.open(fileName, ios::in);
+  if (!file) {
+    throw "bad file";
+  }
+  //cross on the file line-line
+  while (!file.eof()) {
+    getline(file, line);
+    //insert a string and split the strings by ' ', '(', ')', ','
+    while (i < line.size()) {
+      while (line[i] != ' ' && line[i] != '(' && line[i] != ')' && line[i] != ',' ) {
+        temp += line[i];
+        i++;
+      }
+      //insert the strings to the vector.
+      vec.insert(vec.begin(), temp);
+      //initialize the parameters.
+      temp = "";
+      i++;
+    }
+    //initialize i for the next line.(if there is more line)
+    i = 0;
+  }
+
 }
 
-void parser (vector<string> array, unordered_map<string, Command> map) {
+void parser(vector<string> array, unordered_map<string, Command> map) {
   // parse vector and execute through map
 }
