887a1c76c63eedc7c0fc211fe9dc9c96bf9e2791
TalZig <talz2347@gmail.com>
2019-12-10 00:52:52 -0800

    make some new classes

---
+++ commands/DefineVarCommand.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tal on 10.12.2019.
+//
+
+#include "DefineVarCommand.h"
---
+++ commands/DefineVarCommand.h
@@ -0,0 +1,12 @@
+//
+// Created by tal on 10.12.2019.
+//
+
+#ifndef SIMULATOR_COMMANDS_DEFINEVARCOMMAND_H_
+#define SIMULATOR_COMMANDS_DEFINEVARCOMMAND_H_
+
+class DefineVarCommand {
+
+};
+
+#endif //SIMULATOR_COMMANDS_DEFINEVARCOMMAND_H_
