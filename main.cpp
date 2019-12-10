d85b127b8e6c87a6fea1c30c796d3103e1bc3f30
yoelsherwin <yoelsherwin@gmail.com>
2019-12-10 10:54:15 +0200

    opened some command classes

--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <fstream>
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
   std::cout << "yoel's version" << std::endl;
   return 0;
+}
+
+void lexer (FILE* f) {
+  // create array of strings
+  //
 }
