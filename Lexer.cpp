33aa7bcdc9614a6b51a8e95be44335df1e08a64a
yoelsherwin <yoelsherwin@gmail.com>
2019-12-12 14:47:48 +0200

    lexer finished (both function and class, one needs to be removed)

--- Lexer.cpp
+++ Lexer.cpp
@@ -4,5 +4,54 @@
 
 #include "Lexer.h"
 Lexer::Lexer( string fileName) {
+  int i = 0;
+  string line, temp = "";
+  vector<string> vec;
+  fstream file;
+  bool insertedLine = false;
 
+  file.open(fileName, ios::in);
+  if (!file) {
+    cout << "bad file" << endl;
+  }
+
+  //cross on the file line-line
+  while (!file.eof()) {
+    insertedLine = false;
+    getline(file, line);
+    //insert a string and split the strings by ' ', '(', ')', ','
+    while (i < line.size()) {
+      // if the sign is "equal", we reaced a mathematical ass
+      while (i< line.size() && line[i] != '(' && line[i] != ')' && line[i] != ',') {
+        if (line[i] == '=') {
+          i++;
+          if (line[i] == ' ') {
+            i++;
+          }
+          vec.insert(vec.end(), temp);
+          temp = line.substr(i, line.size()-1);
+          vec.insert(vec.end(), temp);
+          i = line.size();
+          insertedLine = true;
+          temp = "";
+          break;
+        }
+        temp += line[i];
+        i++;
+      }
+
+      if (!insertedLine) {
+        //insert the strings to the vector.
+        remove_if(temp.begin(), temp.end(), isspace);
+        vec.insert(vec.end(), temp);
+        //initialize the parameters.
+        temp = "";
+        i++;
+      }
+    }
+    //initialize i for the next line.(if there is more line)
+    i = 0;
+  }
+
+  this->array = vec;
 }
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "map"
 #include "unordered_map"
 #include "vector"
+#include "algorithm"
 
 vector<string> lexer(string fileName);
 
@@ -21,29 +22,46 @@ vector<string> lexer(string fileName) {
   string line, temp = "";
   vector<string> vec;
   fstream file;
+  bool insertedLine = false;
+
   file.open(fileName, ios::in);
   if (!file) {
     cout << "bad file" << endl;
   }
+
   //cross on the file line-line
   while (!file.eof()) {
+    insertedLine = false;
     getline(file, line);
     //insert a string and split the strings by ' ', '(', ')', ','
     while (i < line.size()) {
       // if the sign is "equal", we reaced a mathematical ass
-      if (line[i] == '=') {
-
-      }
       while (i< line.size() && line[i] != '(' && line[i] != ')' && line[i] != ',') {
-
+        if (line[i] == '=') {
+          i++;
+          if (line[i] == ' ') {
+            i++;
+          }
+          vec.insert(vec.end(), temp);
+          temp = line.substr(i, line.size()-1);
+          vec.insert(vec.end(), temp);
+          i = line.size();
+          insertedLine = true;
+          temp = "";
+          break;
+        }
         temp += line[i];
         i++;
       }
-      //insert the strings to the vector.
-      vec.insert(vec.end(), temp);
-      //initialize the parameters.
-      temp = "";
-      i++;
+
+      if (!insertedLine) {
+        //insert the strings to the vector.
+        remove_if(temp.begin(), temp.end(), isspace);
+        vec.insert(vec.end(), temp);
+        //initialize the parameters.
+        temp = "";
+        i++;
+      }
     }
     //initialize i for the next line.(if there is more line)
     i = 0;
