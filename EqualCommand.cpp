e1e36967794baae28edc1fdab9d52b25c036a9bb
yoelsherwin <yoelsherwin@gmail.com>
2019-12-22 11:29:32 +0200

    lexer updates

--- EqualCommand.cpp
+++ EqualCommand.cpp
@@ -7,6 +7,7 @@
 int EqualCommand::execute(vector<string> vector, int index) {
   //insert to the map of the interpreter.
   vector[index].erase(std::remove_if(vector[index].begin(), vector[index].end(), ::isspace), vector[index].end());
+  // do shunting yard on right. concatenate to left and then continue
   Variables::getInstance()->addVarToMap(vector[index]);
 
   return 1;
