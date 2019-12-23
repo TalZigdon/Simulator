a68550eb2f1eb83b1f2c8378858ad37083592acc
TalZig <talz2347@gmail.com>
2019-12-23 16:50:59 +0200

    added delete to parser of FuncCommand

--- FuncCommand.cpp
+++ FuncCommand.cpp
@@ -8,7 +8,8 @@ int FuncCommand::execute(vector<string> vector, int index) {
   double val = Variables::getInstance()->doShuntingYard(vector[index + 1]);
   string temp = this->var + "=" + to_string(val);
   Variables::getInstance()->addVarToMap(temp);
-  new Parser(funcVec);
+  Parser* p = new Parser(funcVec);
+  delete p;
   return 2;
 }
 FuncCommand::FuncCommand(vector<string> vector, string var) {
